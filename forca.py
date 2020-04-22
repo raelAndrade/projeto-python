35ba6efb48fdcaf287a6b1818d882d80510cc78f
israel <israelg.andrade@bol.com.br>
2020-04-22 18:39:39 -0300

    first commit

---
+++ forca.py
@@ -0,0 +1,10 @@
+
+def jogar():
+    print("*********************************")
+    print("Bem vindo ao Jogo de Forca!")
+    print("*********************************")
+
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
