3eb2b74dc1f608329b3ecdd3fb6dba724789b628
israel <israelg.andrade@bol.com.br>
2020-05-28 12:28:19 -0300

    Métodos privados, Static, Métodos de acesso

--- conta.py
+++ conta.py
@@ -1,16 +1,51 @@
 class Conta:
     def __init__(self, numero, titular, saldo, limite):
         print("Construindo objeto ... {}".format(self))
-        self.numero = numero
-        self.titular = titular
-        self.saldo = saldo
-        self.limite = limite
+        self.__numero = numero
+        self.__titular = titular
+        self.__saldo = saldo
+        self.__limite = limite
 
     def extrato(self):
-        print("O saldo é de {} do titular {}".format(self.saldo, self.titular))
+        print("O saldo é de {} do titular {}".format(self.__saldo, self.__titular))
 
     def deposita(self, valor):
-        self.saldo += valor
+        self.__saldo += valor
+
+    def __pode_sacar(self, valor_a_sacar):
+        valor_disponivel_a_sacar = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_disponivel_a_sacar
 
     def saca(self, valor):
-        self.saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
+
+    def transfere(self, valor, destino):
+        self.saca(valor)
+        destino.deposita(valor)
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @titular.setter
+    def titular(self, titular):
+        self.__titular = titular
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': '237'}
