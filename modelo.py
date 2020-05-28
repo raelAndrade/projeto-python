14d11cec2bc34b5a53674da1daa3e30d89995f3a
israel <israelg.andrade@bol.com.br>
2020-05-28 14:19:28 -0300

    Orientação a Objetoss segunda parte

---
+++ modelo.py
@@ -0,0 +1,12 @@
+class Filme:
+    def __init__(self, nome, ano, duracao, likes):
+        self.nome = nome
+        self.ano = ano
+        self.duracao = duracao
+        self.likes = likes
+
+class Serie:
+    def __init__(self, nome, ano, temporadas):
+        self.nome = nome
+        self.ano = ano
+        self.temporadas = temporadas
